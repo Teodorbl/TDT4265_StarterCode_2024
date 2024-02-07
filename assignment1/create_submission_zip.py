1ce250280d154d15c40a7b399cf26ad9a8fab24b
HenrikLogJacobsen <loghenrik@gmail.com>
2024-02-07 16:36:29 +0100

    submission 1 created

--- assignment1/create_submission_zip.py
+++ assignment1/create_submission_zip.py
@@ -44,7 +44,7 @@ def select_file(filename, extension):
 files_added = []
 with zipfile.ZipFile(zipfile_path, "w") as fp:
     for filename, extensions in files_to_include.items():
-        filepath = select_file(filename, extensions)
+        filepath = select_file("./assignment1/"+filename, extensions)
         assert os.path.isfile(filepath),\
             f"Did not find path: {filepath}"
         fp.write(filepath)
