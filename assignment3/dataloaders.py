4e3416eb7093059168020dc24af4a11781f2cb6f
hennabenna11 <loghenrik@gmail.com>
2024-03-04 15:34:30 +0100

    dataload update

--- assignment3/dataloaders.py
+++ assignment3/dataloaders.py
@@ -8,9 +8,11 @@ import numpy as np
 import pathlib
 np.random.seed(0)
 
-mean = (0.5, 0.5, 0.5)
-std = (.25, .25, .25)
+# mean = (0.5, 0.5, 0.5)
+# std = (.25, .25, .25)
 
+mean= (0.485, 0.456, 0.406)
+std= (0.229, 0.224, 0.225)
 
 def get_data_dir():
     server_dir = pathlib.Path("/work/datasets/cifar10")
@@ -19,7 +21,7 @@ def get_data_dir():
     return "data/cifar10"
 
 
-def load_cifar10(batch_size: int, validation_fraction: float = 0.1,
+def load_cifar10(batch_size: int, validation_fraction: float = 0.1
                  ) -> typing.List[torch.utils.data.DataLoader]:
     # Note that transform train will apply the same transform for
     # validation!
