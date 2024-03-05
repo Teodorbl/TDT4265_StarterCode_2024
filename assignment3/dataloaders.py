bb5f19af10f07a2514dbca53cc64fdc43c6903c1
Teodor Lindell <teodorbl@stud.ntnu.no>
2024-03-05 16:28:55 +0100

    task3 data augmentation

--- assignment3/dataloaders.py
+++ assignment3/dataloaders.py
@@ -8,9 +8,14 @@ import numpy as np
 import pathlib
 np.random.seed(0)
 
-mean = (0.5, 0.5, 0.5)
-std = (.25, .25, .25)
+# Given:
+# mean = (0.5, 0.5, 0.5)
+# std = (.25, .25, .25)
 
+mean = (0.4914, 0.4822, 0.4465)
+std = (0.2023, 0.1994, 0.2010)
+
+# Task 4:
 # mean= (0.485, 0.456, 0.406)
 # std= (0.229, 0.224, 0.225)
 
@@ -28,20 +33,17 @@ def load_cifar10(batch_size: int, validation_fraction: float = 0.1, shrink: bool
 
     transform_train = transforms.Compose([
         transforms.ToTensor(),
-        #transforms.Resize((112, 112)) if shrink else transforms.Resize((224, 224)),
         transforms.Normalize(mean, std),
     ])
 
     transform_train_augmented = transforms.Compose([
-        # transforms.RandomResizedCrop(size=(32, 32), scale=(0.8, 1.0), ratio=(1, 1)),
+        transforms.RandomResizedCrop(size=(32, 32), scale=(0.8, 1.0), ratio=(1, 1), antialias=True),
         transforms.ToTensor(),
-        # transforms.Resize((112, 112)) if shrink else transforms.Resize((224, 224)),
         transforms.Normalize(mean, std),
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        # transforms.Resize((112, 112)) if shrink else transforms.Resize((224, 224)),
         transforms.Normalize(mean, std),
     ])
     data_train = datasets.CIFAR10(get_data_dir(),
@@ -49,21 +51,21 @@ def load_cifar10(batch_size: int, validation_fraction: float = 0.1, shrink: bool
                                   download=True,
                                   transform=transform_train)
     
-    # data_train_augmented = datasets.CIFAR10(get_data_dir(),
-    #                               train=True,
-    #                               download=True,
-    #                               transform=transform_train_augmented)
+    data_train_augmented = datasets.CIFAR10(get_data_dir(),
+                                  train=True,
+                                  download=True,
+                                  transform=transform_train_augmented)
 
     data_test = datasets.CIFAR10(get_data_dir(),
                                  train=False,
                                  download=True,
                                  transform=transform_test)
     
-    # data_train_combined = ConcatDataset([data_train, data_train_augmented])
+    data_train_combined = ConcatDataset([data_train, data_train_augmented])
 
-    indices = list(range(len(data_train)))
-    #print("Number of indices: ", len(indices))
-    split_idx = int(np.floor(validation_fraction * len(data_train)))
+    indices = list(range(len(data_train_combined)))
+    print("Number of indices: ", len(indices))
+    split_idx = int(np.floor(validation_fraction * len(data_train_combined)))
 
     val_indices = np.random.choice(indices, size=split_idx, replace=False)
     train_indices = list(set(indices) - set(val_indices))
@@ -71,13 +73,13 @@ def load_cifar10(batch_size: int, validation_fraction: float = 0.1, shrink: bool
     train_sampler = SubsetRandomSampler(train_indices)
     validation_sampler = SubsetRandomSampler(val_indices)
 
-    dataloader_train = torch.utils.data.DataLoader(data_train,
+    dataloader_train = torch.utils.data.DataLoader(data_train_combined,
                                                    sampler=train_sampler,
                                                    batch_size=batch_size,
                                                    num_workers=2,
                                                    drop_last=True)
 
-    dataloader_val = torch.utils.data.DataLoader(data_train,
+    dataloader_val = torch.utils.data.DataLoader(data_train_combined,
                                                  sampler=validation_sampler,
                                                  batch_size=batch_size,
                                                  num_workers=2)
